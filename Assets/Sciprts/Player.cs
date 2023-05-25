8bb583b9963d788f24aa8b3ea6be93b7069ff6fa
YamanaSousuke <103532568+YamanaSousuke@users.noreply.github.com>
2023-05-25 22:54:46 +0900

    [add] Add player script and animation
    プレイヤーの動きを制御するスクリプトとアニメーションの追加

---
+++ Assets/Sciprts/Player.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Novel
+{
+    // プレイヤーの制御
+    public class Player : MonoBehaviour
+    {
+        // 移動スピード
+        [SerializeField] private float speed = 0.0f;
+
+        // 物理特性にる制御
+        private new Rigidbody2D rigidbody = null;
+        // アニメーションの制御
+        private Animator animator = null;
+        // 画像の表示を制御する
+        private SpriteRenderer sprite = null;
+        // 移動
+        private Vector2 move = new();
+
+        // アニメーションID
+        private static readonly int horizontalId = Animator.StringToHash("Horizontal");
+        private static readonly int verticalId = Animator.StringToHash("Vertical");
+        private static readonly int isMoveId = Animator.StringToHash("IsMove");
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            sprite = GetComponent<SpriteRenderer>();
+        }
+
+        private void FixedUpdate()
+        {
+            // 移動
+            rigidbody.velocity = move.normalized * speed;
+        }
+
+        // 移動の入力を受け取る
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            move.x = context.ReadValue<Vector2>().x;
+            move.y = context.ReadValue<Vector2>().y;
+
+            // 右に移動したとき
+            if (move.x > 0.0f)
+            {
+                sprite.flipX = false;
+            }
+            // 左に移動したら画像を反転させる
+            else if (move.x < 0.0f)
+            {
+                sprite.flipX = true;
+            }
+
+            // 入力に応じてアニメーションを切り替える
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                animator.SetBool(isMoveId, true);
+                
+                break;
+
+                case InputActionPhase.Performed:
+                animator.SetFloat(horizontalId, move.x);
+                animator.SetFloat(verticalId, move.y);
+                break;
+
+                case InputActionPhase.Canceled:
+                animator.SetBool(isMoveId, false);
+                break;
+            }
+        }
+    }
+}
